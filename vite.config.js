761b9f8d466ad48abc2aaeeb24ed910c298c8680
Fabian <acostahf4@gmail.com>
2022-09-27 11:57:03 -0500

    first commit

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()]
+// })
+
+export default defineConfig({
+	plugins: [cssInjectedByJsPlugin()],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: undefined,
+			},
+		},
+	},
+});
